a32837c76ced687ad5fdb01406964693d416b8fc
João Paulo Ribeiro <tabulti@gmail.com>
2017-11-08 15:32:46 -0300

    in progress

---
+++ app/src/main/java/com/simulando/joaopaulodribeiro/simulando/model/AlternativeK.kt
@@ -0,0 +1,9 @@
+package com.simulando.joaopaulodribeiro.simulando.model
+
+/**
+ * Created by joao.paulo.d.ribeiro on 08/11/2017.
+ */
+class AlternativeK constructor(letter : String, selected : Boolean){
+    val letter: String = letter
+    val selected: Boolean = selected
+}
---
+++ app/src/main/java/com/simulando/joaopaulodribeiro/simulando/model/student/StudentK.kt
@@ -0,0 +1,7 @@
+package com.simulando.joaopaulodribeiro.simulando.model.student
+
+/**
+ * Created by joao.paulo.d.ribeiro on 08/11/2017.
+ */
+class StudentK (val id: Int, val name: String, val created_at: String, val updated_at: String,
+               val deleted_at: String, val school_id: String, val user_id: Int)
